63debf107a61ee57af3942d274b9f527f90e91c1
julie17-d <derbaljulie@gmail.com>
2023-01-03 17:07:05 +0100

    feat: button to change view + display array

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.go_second_page)
+        button.setOnClickListener{
+            val intent = Intent(this, second_page::class.java)
+            startActivity(intent)
+        }
     }
 }
