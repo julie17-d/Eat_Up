bcbc7eeda5151262adf508e5c2d5f641b1ccad2f
julie17-d <derbaljulie@gmail.com>
2023-01-10 15:38:56 +0100

    feat: display search results

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.go_home)
+        val button = findViewById<TextView>(R.id.go_home)
         button.setOnClickListener{
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
